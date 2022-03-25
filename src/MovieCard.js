0650392a49b8faa6cd0e5e6b62df717de84bcc8f
Rizky1408 <agungdwisatria23@gmail.com>
2022-03-26 00:10:30 +0800

    second commit

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -2,20 +2,19 @@ import React, { Component } from "react";
 
 export default class MovieCard extends Component {
     render() {
-        const { movie } = this.props;
+        const { movie } = this.props
         return (
             <div className="col-12 my-3">
                 <div class="card h-100">
-                    <img src={movie.image_url} class="card-img-top" alt="skilvul" />
+                    <img
+                        src={movie.image_url}
+                        class="card-img-top"
+                        alt="skilvul"
+                    />
                     <div class="card-body">
                         <h5 class="card-title">{movie.title}</h5>
                         <h6 class="card-subtitle mb-2">
-                            <span
-                                class={`badge ${movie.type === "TV" ? "bg-danger" : "bg-success"
-                                    } `}
-                            >
-                                {movie.type}
-                            </span>
+                            <span class={`badge ${movie.type === 'TV' ? 'bg-danger' : 'bg-success'} `}>{movie.type}</span>
                         </h6>
                         <p class="card-text">
                             Some quick example text to build on the card title and make up the
@@ -23,7 +22,10 @@ export default class MovieCard extends Component {
                         </p>
                     </div>
                     <div class="card-body">
-                        <a href={movie.url} class="btn btn-primary w-100 text-uppercase">
+                        <a
+                            href={movie.url}
+                            class="btn btn-primary w-100 text-uppercase"
+                        >
                             readmore
                         </a>
                     </div>
